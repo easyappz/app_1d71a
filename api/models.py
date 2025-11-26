ed8edd76444f1f05fdecc2aadae96812a526a8cc
IbragimKhaniev <newinterny@gmail.com>
2025-11-26 19:39:30 +0300

    Add database models and Pillow dependency

--- api/models.py
+++ api/models.py
@@ -1,3 +1,150 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password, check_password
+from django.utils import timezone
 
-# Create your models here.
+
+class Member(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    password_hash = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=150, blank=True, default='')
+    last_name = models.CharField(max_length=150, blank=True, default='')
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    bio = models.TextField(blank=True, default='')
+    is_online = models.BooleanField(default=False)
+    last_seen = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'members'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.username
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def set_password(self, raw_password):
+        self.password_hash = make_password(raw_password)
+
+    def check_password(self, raw_password):
+        return check_password(raw_password, self.password_hash)
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        Member,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+    content = models.TextField()
+    image = models.ImageField(upload_to='posts/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'posts'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'Post by {self.author.username} at {self.created_at}'
+
+
+class Like(models.Model):
+    user = models.ForeignKey(
+        Member,
+        on_delete=models.CASCADE,
+        related_name='likes'
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='likes'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'likes'
+        unique_together = ('user', 'post')
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.user.username} likes {self.post.id}'
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        Member,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'comments'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'Comment by {self.author.username} on post {self.post.id}'
+
+
+class Subscription(models.Model):
+    subscriber = models.ForeignKey(
+        Member,
+        on_delete=models.CASCADE,
+        related_name='subscriptions'
+    )
+    target = models.ForeignKey(
+        Member,
+        on_delete=models.CASCADE,
+        related_name='subscribers'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'subscriptions'
+        unique_together = ('subscriber', 'target')
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.subscriber.username} follows {self.target.username}'
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(
+        Member,
+        on_delete=models.CASCADE,
+        related_name='sent_messages'
+    )
+    receiver = models.ForeignKey(
+        Member,
+        on_delete=models.CASCADE,
+        related_name='received_messages'
+    )
+    content = models.TextField()
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'messages'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'Message from {self.sender.username} to {self.receiver.username}'
