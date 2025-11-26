70e2e93a90d7fc8897d5f6c24af8789e7d1db0cf
IbragimKhaniev <newinterny@gmail.com>
2025-11-26 19:40:01 +0300

    Create all serializers according to OpenAPI specification

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,129 @@
 from rest_framework import serializers
+from api.models import Member, Post, Comment, Message, Subscription, Like
 
 
-class MessageSerializer(serializers.Serializer):
-    message = serializers.CharField(max_length=200)
-    timestamp = serializers.DateTimeField(read_only=True)
+class MemberShortSerializer(serializers.ModelSerializer):
+    """Short serializer for nested user representation"""
+    class Meta:
+        model = Member
+        fields = ['id', 'username', 'first_name', 'last_name', 'avatar', 'is_online']
+        read_only_fields = ['id', 'username', 'first_name', 'last_name', 'avatar', 'is_online']
+
+
+class MemberSerializer(serializers.ModelSerializer):
+    """Full member profile serializer"""
+    class Meta:
+        model = Member
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'avatar', 'bio', 'is_online', 'last_seen', 'created_at']
+        read_only_fields = ['id', 'username', 'email', 'created_at']
+
+
+class MemberRegistrationSerializer(serializers.ModelSerializer):
+    """Serializer for user registration"""
+    password = serializers.CharField(write_only=True, min_length=8, style={'input_type': 'password'})
+    
+    class Meta:
+        model = Member
+        fields = ['username', 'email', 'password', 'first_name', 'last_name']
+        extra_kwargs = {
+            'username': {'min_length': 3, 'max_length': 150},
+            'first_name': {'max_length': 150, 'required': True},
+            'last_name': {'max_length': 150, 'required': True},
+        }
+    
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        member = Member(**validated_data)
+        member.set_password(password)
+        member.save()
+        return member
+
+
+class MemberLoginSerializer(serializers.Serializer):
+    """Serializer for user login"""
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True, write_only=True, style={'input_type': 'password'})
+
+
+class PostCreateSerializer(serializers.ModelSerializer):
+    """Serializer for creating posts"""
+    class Meta:
+        model = Post
+        fields = ['content', 'image']
+        extra_kwargs = {
+            'content': {'min_length': 1},
+            'image': {'required': False, 'allow_null': True},
+        }
+
+
+class PostSerializer(serializers.ModelSerializer):
+    """Full post serializer with related data"""
+    author = MemberShortSerializer(read_only=True)
+    likes_count = serializers.SerializerMethodField()
+    comments_count = serializers.SerializerMethodField()
+    is_liked = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Post
+        fields = ['id', 'author', 'content', 'image', 'likes_count', 'comments_count', 'is_liked', 'created_at']
+        read_only_fields = ['id', 'author', 'created_at']
+    
+    def get_likes_count(self, obj):
+        return obj.likes.count()
+    
+    def get_comments_count(self, obj):
+        return obj.comments.count()
+    
+    def get_is_liked(self, obj):
+        request = self.context.get('request')
+        if request and hasattr(request, 'user') and request.user.is_authenticated:
+            return obj.likes.filter(user=request.user).exists()
+        return False
+
+
+class CommentCreateSerializer(serializers.ModelSerializer):
+    """Serializer for creating comments"""
+    class Meta:
+        model = Comment
+        fields = ['content']
+        extra_kwargs = {
+            'content': {'min_length': 1},
+        }
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    """Full comment serializer with author data"""
+    author = MemberShortSerializer(read_only=True)
+    post = serializers.PrimaryKeyRelatedField(read_only=True)
+    
+    class Meta:
+        model = Comment
+        fields = ['id', 'author', 'post', 'content', 'created_at']
+        read_only_fields = ['id', 'author', 'post', 'created_at']
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    """Serializer for messages"""
+    class Meta:
+        model = Message
+        fields = ['id', 'sender', 'receiver', 'content', 'is_read', 'created_at']
+        read_only_fields = ['id', 'sender', 'receiver', 'is_read', 'created_at']
+
+
+class DialogSerializer(serializers.Serializer):
+    """Serializer for dialog list"""
+    id = serializers.IntegerField()
+    participant = MemberShortSerializer()
+    last_message = MessageSerializer(allow_null=True)
+    unread_count = serializers.IntegerField()
+
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    """Serializer for subscriptions"""
+    subscriber_id = serializers.IntegerField(source='subscriber.id', read_only=True)
+    subscribed_to_id = serializers.IntegerField(source='target.id', read_only=True)
+    
+    class Meta:
+        model = Subscription
+        fields = ['id', 'subscriber_id', 'subscribed_to_id', 'created_at']
+        read_only_fields = ['id', 'subscriber_id', 'subscribed_to_id', 'created_at']
